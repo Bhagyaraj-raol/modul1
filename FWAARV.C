1a9fdf8043767ea97ce890b7b7faac3169667f23
Raol Bhagyaraj <raolforam350@gmail.com>
2025-07-25 20:15:07 +0530

    Add files via upload

---
+++ FWAARV.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int multipiy(int,int);
+
+void main()
+{
+	int result;
+	clrscr();
+	result = multiply(4,5);
+	printf("multiplication = %d",result);
+	getch();
+}
+
+int multiply(int a,int b)
+{
+	return a*b;
+}
+
+
+
